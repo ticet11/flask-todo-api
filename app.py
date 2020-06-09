beae556a1c78c3e770c1a184d317f322c46797e3
Brian Kozub <ticet11@gmail.com>
2020-06-09 11:56:18 -0600

    reformated code for readability.

--- app.py
+++ app.py
@@ -42,12 +42,9 @@ def hello():
 def add_todo():
     title = request.json['title']
     done = request.json['done']
-
     new_todo = Todo(title, done)
-
     db.session.add(new_todo)
     db.session.commit()
-
     todo = Todo.query.get(new_todo.id)
     return todo_schema.jsonify(todo)
 
@@ -56,7 +53,6 @@ def add_todo():
 def get_todos():
     all_todos = Todo.query.all()
     result = todos_schema.dump(all_todos)
-
     return jsonify(result)
 
 
@@ -68,12 +64,12 @@ def update_todo(id):
     db.session.commit()
     return todo_schema.jsonify(todo)
 
+
 @app.route('/todo/<id>', methods=['DELETE'])
 def delete_todo(id):
     todo = Todo.query.get(id)
     db.session.delete(todo)
     db.session.commit()
-
     return jsonify('Item deleted.')
 
 
