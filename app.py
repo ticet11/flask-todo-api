4436e893332c38cdd0c8b670e9fcc6a0621dde7d
Brian Kozub <ticet11@gmail.com>
2020-06-09 11:35:27 -0600

    Create todo post request functional.

--- app.py
+++ app.py
@@ -37,6 +37,19 @@ todos_schema = TodoSchema(many=True)
 def hello():
     return 'Hello World!'
 
+@app.route('/todo', methods=['POST'])
+def add_todo():
+    title = request.json['title']
+    done = request.json['done']
+
+    new_todo = Todo(title, done)
+
+    db.session.add(new_todo)
+    db.session.commit()
+
+    todo = Todo.query.get(new_todo.id)
+    return todo_schema.jsonify(todo)
+
 
 if __name__ == '__main__':
     app.debug = True
